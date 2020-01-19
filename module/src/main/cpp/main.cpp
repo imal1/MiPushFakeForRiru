8a2714eacfd4a9ac1366bc5cc762ffb8fe106f5e
tianshuo.zts <tianshuo.zts@alipay.com>
2020-01-19 22:02:11 +0800

    mipush fake

--- module/src/main/cpp/main.cpp
+++ module/src/main/cpp/main.cpp
@@ -1,7 +1,35 @@
 #include <jni.h>
 #include <sys/types.h>
+#include <stdio.h>
+#include <string>
+#include <vector>
+#include <unistd.h>
+#include "logging.h"
+#include "hook.h"
+
+#define CONFIG_PATH "/data/misc/riru/modules/mipush_fake"
+
+#define FAKE_CONFIGURATION_GLOBAL "/data/misc/riru/modules/mipush_fake/packages/ALL"
 
 // You can remove functions you don't need
+static char package_name[256];
+static bool enable_hook;
+static int uid;
+
+static std::vector<std::string> globalPkgBlackList = {"com.google.android",
+                                             "de.robv.android.xposed.installer",
+                                             "com.xiaomi.xmsf",
+                                             "com.tencent.mm",
+                                             "top.trumeet.mipush"};
+
+
+bool isAppNeedHook(JNIEnv *pEnv, jstring pJstring);
+
+void injectBuild(JNIEnv *pEnv);
+
+static void appProcessPre(JNIEnv *env, jint _uid, jstring appDataDir, jstring packageName);
+
+static void appProcessPost(JNIEnv *env);
 
 extern "C" {
 #define EXPORT __attribute__((visibility("default"))) __attribute__((used))
@@ -12,11 +40,14 @@ EXPORT void nativeForkAndSpecializePre(
         jstring *instructionSet, jstring *appDataDir, jstring *packageName,
         jobjectArray *packagesForUID, jstring *sandboxId) {
     // packageName, packagesForUID, sandboxId are added from Android Q beta 2, removed from beta 5
+
+    appProcessPre(env, *_uid, *appDataDir, *packageName);
 }
 
 EXPORT int nativeForkAndSpecializePost(JNIEnv *env, jclass clazz, jint res) {
     if (res == 0) {
         // in app process
+        appProcessPost(env);
     } else {
         // in zygote process, res is child pid
         // don't print log here, see https://github.com/RikkaApps/Riru/blob/77adfd6a4a6a81bfd20569c910bc4854f2f84f5e/riru-core/jni/main/jni_native_method.cpp#L55-L66
@@ -32,11 +63,15 @@ EXPORT __attribute__((visibility("default"))) void specializeAppProcessPre(
     // this is added from Android Q beta, but seems Google disabled this in following updates
 
     // packageName, packagesForUID, sandboxId are added from Android Q beta 2, removed from beta 5
+
+    appProcessPre(env, *_uid, *appDataDir, *packageName);
 }
 
 EXPORT __attribute__((visibility("default"))) int specializeAppProcessPost(
         JNIEnv *env, jclass clazz) {
     // this is added from Android Q beta, but seems Google disabled this in following updates
+
+    appProcessPost(env);
     return 0;
 }
 
@@ -65,4 +100,105 @@ EXPORT int shouldSkipUid(int uid) {
 EXPORT void onModuleLoaded() {
     // called when the shared library of Riru core is loaded
 }
+}
+
+void injectBuild(JNIEnv *env) {
+    if (env == nullptr) {
+        LOGW("failed to inject android.os.Build for %s due to env is null", package_name);
+        return;
+    }
+    LOGI("inject android.os.Build for %s ", package_name);
+
+    jclass build_class = env->FindClass("android/os/Build");
+    if (build_class == nullptr) {
+        LOGW("failed to inject android.os.Build for %s due to build is null", package_name);
+        return;
+    }
+
+    jstring new_str = env->NewStringUTF("Xiaomi");
+
+    jfieldID brand_id = env->GetStaticFieldID(build_class, "BRAND", "Ljava/lang/String;");
+    if (brand_id != nullptr) {
+        env->SetStaticObjectField(build_class, brand_id, new_str);
+    }
+
+    jfieldID manufacturer_id = env->GetStaticFieldID(build_class, "MANUFACTURER", "Ljava/lang/String;");
+    if (manufacturer_id != nullptr) {
+        env->SetStaticObjectField(build_class, manufacturer_id, new_str);
+    }
+
+    jfieldID product_id = env->GetStaticFieldID(build_class, "PRODUCT", "Ljava/lang/String;");
+    if (product_id != nullptr) {
+        env->SetStaticObjectField(build_class, product_id, new_str);
+    }
+
+    if(env->ExceptionCheck())
+    {
+        env->ExceptionClear();
+    }
+
+    env->DeleteLocalRef(new_str);
+}
+
+bool isAppNeedHook(JNIEnv *env, jstring jAppDataDir) {
+    if (jAppDataDir == nullptr) {
+        return false;
+    }
+
+    const char *appDataDir = env->GetStringUTFChars(jAppDataDir, nullptr);
+
+    int user = 0;
+    while (true) {
+        // /data/user/<user_id>/<package>
+        if (sscanf(appDataDir, "/data/%*[^/]/%d/%s", &user, package_name) == 2)
+            break;
+
+        // /mnt/expand/<id>/user/<user_id>/<package>
+        if (sscanf(appDataDir, "/mnt/expand/%*[^/]/%*[^/]/%d/%s", &user, package_name) == 2)
+            break;
+
+        // /data/data/<package>
+        if (sscanf(appDataDir, "/data/%*[^/]/%s", package_name) == 1)
+            break;
+
+        package_name[0] = '\0';
+        return false;
+    }
+    env->ReleaseStringUTFChars(jAppDataDir, appDataDir);
+
+    std::string pkgName = package_name;
+    for (auto &s : globalPkgBlackList) {
+        if (pkgName.find(s) != std::string::npos) {
+            return false;
+        }
+    }
+
+    if (access(FAKE_CONFIGURATION_GLOBAL, F_OK) == 0) {
+        return true;
+    }
+
+    if (access(CONFIG_PATH "/packages", R_OK) == 0) {
+        char path[PATH_MAX];
+        snprintf(path, PATH_MAX, CONFIG_PATH "/packages/%d.%s", user, package_name);
+        return access(path, F_OK) == 0;
+    }
+
+    return false;
+}
+
+
+
+static void appProcessPre(JNIEnv *env, jint _uid, jstring appDataDir, jstring packageName) {
+
+    uid = _uid;
+    enable_hook = isAppNeedHook(env, appDataDir);
+}
+
+
+static void appProcessPost(JNIEnv *env) {
+
+    if (enable_hook) {
+        injectBuild(env);
+        install_hook(package_name, uid / 100000);
+    }
 }
